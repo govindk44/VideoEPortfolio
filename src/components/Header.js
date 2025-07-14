ef45514dbdc988a326616ed3b1b97963d163b343
Govind k <132933148+govindk44@users.noreply.github.com>
2025-07-14 22:46:38 +0530

    Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import './Header.css';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 function Header() {
   const [scrolled, setScrolled] = useState(false);
